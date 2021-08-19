6278c258e5642ec4bca9c29a451e63ff704d183c
ArtDinWin <andsmirastrav@mail.ru>
2021-08-19 22:13:44 +0300

    Добавил интерактив на сайт с помощью языка JavaScript и библиотеки jQuery. Написал скрипт подсветки этажей и изменение счетчика этажей.

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+   // console.log('Сайт готов к манипуляциям');
+var currentFloor = 2; // переменная где хранится текущий этаж
+var currentUp = $(".counter-up");// увеличение этажа по кнопке вверх
+var currentDown = $(".counter-down");// уменьшение этажа по кнопке вниз
+var floorPath = $(".home-image path"); // Каждый отдельный этаж в svg
+// on('click' при клике или on('mouseover' при наведении
+   floorPath.on('mouseover', function(){
+       floorPath.removeClass('current-floor');  // удаляем активный класс у этажей
+currentFloor = $(this).attr("data-floor"); // получаем значение текущего этажа
+    $(".counter").text(currentFloor); // записываем значение этажа в счетчик
+    $(`[data-floor=${currentFloor}]`).toggleClass("current-floor"); // подсвечиваем текущий этаж, чтобы после ухода с этажа он был подсвеченный
+});
+
+currentUp.on('click', function(){ // отслеживаем клик по кнопке вверх
+if(currentFloor<18) { // проверяем  значение этажа, оно не должно быть больше 18
+    currentFloor++; // прибовляем один этаж
+usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false}); // форматируем номер этажа с 1 например в формат 01 
+$(".counter").text(usCurrentFloor); // записываем значение этажа в счетчик
+//$(".home-image path");
+floorPath.removeClass('current-floor'); // удаляем активный класс у этажей
+$(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // подсвечиваем текущий этаж
+}
+});
+
+currentDown.on('click', function(){
+if(currentFloor > 2) {
+    currentFloor--;
+usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false});
+$(".counter").text(usCurrentFloor);
+//$(".home-image path");
+floorPath.removeClass('current-floor');
+$(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+}
+});
+
+
+});
